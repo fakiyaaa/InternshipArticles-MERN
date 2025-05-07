3ed9febffc5ce970c83e7c3868a16ae9d36af490
fakiyaaa <149144145+fakiyaaa@users.noreply.github.com>
2025-05-07 13:51:01 -0700

    commit

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -13,11 +16,21 @@ mongoose.connect('mongodb+srv://fakiya:CQKVmRzYMwWqGo5o@cluster0.njwfids.mongodb
 app.post('/register', async (req,res) =>{
   const {username, password} = req.body;
   try{
-    const userDoc = await User.create({username, password});
+    const userDoc = await User.create({
+      username, 
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch(e) {
+    console.log(e);
     res.status(400).json(e);
   }
 
+});
+
+app.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  const userDoc = await User.findOne({username});
+  res.json(userDoc);
 });
 app.listen(4000);
