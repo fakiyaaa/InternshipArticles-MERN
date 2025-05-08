10a6a1e3e50675e844b1a53ef841ab7fe5864093
fakiyaaa <149144145+fakiyaaa@users.noreply.github.com>
2025-05-07 17:54:44 -0700

    added backend between login and register

--- api/index.js
+++ api/index.js
@@ -4,11 +4,15 @@ const app = express();
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'kqjdwqnxjwxns2';
 
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 mongoose.connect('mongodb+srv://fakiya:CQKVmRzYMwWqGo5o@cluster0.njwfids.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -31,6 +35,29 @@ app.post('/register', async (req,res) =>{
 app.post('/login', async (req, res) => {
   const {username, password} = req.body;
   const userDoc = await User.findOne({username});
-  res.json(userDoc);
+  
+  const passOk = bcrypt.compareSync(password, userDoc.password)
+  if (passOk){
+    //logged in
+    jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie('token', token).json('ok');
+    });
+  } else {
+    res.status(400).json('Wrong Credentials. Try Again Please');
+  }
+});
+
+app.get('/profile', (req, res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
 });
 app.listen(4000);
